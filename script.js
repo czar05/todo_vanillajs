7afee62ff431d68ffb393067f7ec8fcf26192365
Deepak patel <36337084+czar05@users.noreply.github.com>
2021-11-26 21:16:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 let btn = document.getElementById("addToDo");
 let container = document.getElementById("toDoContainer");
 let inputField = document.getElementById("inputField");
+let counter = document.getElementById("count");
+let task_completed = document.getElementById("task_completed");
 let count = 0;
 btn.addEventListener("click", function () {
   var list = document.createElement("P");
-  //list.classList.add("paragraph-styling");
+
   list.className = "ul1";
  
   count += 1;
-  console.log(count);
+  counter.innerText  = "Total Tasks:" + count;
   
   list.innerText = inputField.value;
  
-//  var checkbox = document.createElement("input");
-//  checkbox.type = "checkbox";
-//  checkbox.value = 1;
-//  checkbox.name = "todo[]";
-//  checkbox.className="check";
-//  list.appendChild(checkbox);
+
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7"); // \u00D7
   span.className = "close";
   span.appendChild(txt);
   list.setAttribute("background-color", "white");
-  // paragraph.appendChild(span);
+
   if (inputField.value != "") {
     list.appendChild(span);
   }
@@ -34,6 +31,7 @@ btn.addEventListener("click", function () {
   
   list.addEventListener("click", function () {
     list.style.textDecoration = "line-through";
+ 
   });
   list.addEventListener("dblclick", function () {
     list.style.textDecoration = "none";
@@ -47,14 +45,14 @@ btn.addEventListener("click", function () {
     var div = this.parentElement;
     div.style.display = "none";
     count = count -1;
+    counter.innerText  = "Total Tasks:" + count;
     console.log(count);
- 
-  
     
   });
+ 
   
-  // var addhere = document.getElementById('add_here');
-  // addhere.textContent = `Total tasks : ${count}`;
 });
+
+
 
 
